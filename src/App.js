24efd86117dd5725ff25f75e6bb19700c1dcdc92
xinyuancai <xinyuancai@tencent.com>
2021-09-18 00:31:31 +0800

    feat: 接入彩虹屁API & 背景渐变

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Input } from 'antd';
+import { Layout } from 'antd';
+import './App.css'
+const { Search } = Input;
+const { Header, Footer, Content, Sider } = Layout;
+
+const onSearch = value => console.log(value);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header></Header>
+        <Layout>
+        <Sider></Sider>
+        <Content>
+          <Search
+          placeholder="input search text"
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={onSearch}
+        />
+        </Content>
+        <Sider></Sider>
+        </Layout>
+        <Footer></Footer>
+      </Layout>
+
     </div>
   );
 }
